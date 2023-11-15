11f6e2a99fa18fa8a408e26d62af17a4db4094fd
araaancg <carpinteroaranchag@gmail.com>
2023-11-15 21:37:45 +0100

    fix: dinamic routes & capitalize names

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import json
 import shutil
 
+proyectPath = "../reusable-react-components/src"
+
 # FUNCIONES
 def getData(jsonFile):
     with open(jsonFile, encoding="utf8") as file:
@@ -13,6 +15,10 @@ def createFile(fileName, text):
         file.write(text)
     file.close()
 
+def capitalizeCamelCase(str):
+    newStr = "".join([a.capitalize() for a in str.split("")])
+    return newStr
+
 def changeExtension(fileName, newExtension, **fileType):
     if fileType.get("module"):
         os.rename(fileName, f'{fileName.split(".")[0]}.module.{newExtension}')
@@ -22,34 +28,30 @@ def changeExtension(fileName, newExtension, **fileType):
         os.rename(fileName, f'{fileName.split(".")[0]}.{newExtension}')
 
 def writeText(name, fileType):
-    name = name.capitalize()
     if fileType == "page":
-        return 'import styles from "./' + name + '.module.scss"; export default function ' + name+'():JSX.Element {return (<main><p>' + name + '</p></main>)}'
+        return 'import styles from "./' + name + '.module.scss"; export default function ' + name +'():JSX.Element {return (<main><p>' + name + '</p></main>)}'
     elif fileType == "layout":
-        return 'export default function RootLayout({children,}: {children: React.ReactNode;}): JSX.Element {return <main className="' + name.lower() + '-main-container">{ children }</main>;}'
+        return 'export default function RootLayout({children,}: {children: React.ReactNode;}): JSX.Element {return <main className="' + name + '-main-container">{ children }</main>;}'
     elif fileType == "functional":
-        return 'import template' + name + ' from "./' + name + '.template";const ' + name + ' =()=>{ return template' + name + '();}export default ' + name + ';'
+        return 'import Template' + name + ' from "./' + name + '.template";const ' + name + ' =()=>{ return Template' + name + '();};export default ' + name + ';'
     elif fileType == "template":
-        return 'import React from "react";import "./' + name + '.module.scss";const template' + name + ' = (): JSX.Element => {return <div></div>;};export default template' + name + ';'
+        return 'import React from "react";import styles from "./' + name + '.module.scss";const Template' + name + ' = (): JSX.Element => {return <div></div>;};export default Template' + name + ';'
 
 # PASO 1: CREAR CARPETA /src
-# dentro de la carpeta, crear las correspondientes 'app' y 'components'
+# dentro de la carpeta, crear carpeta 'componentes'
 
-os.mkdir('src')
-os.makedirs('./src/components')
-os.makedirs('./src/app')
+os.makedirs(f'{proyectPath}/components')
 
 # PASO 2: CREAMOS LOS ARCHIVOS BÁSICOS PARA ./app
 #  - page.tsx
 #  - layout.tsx
 #  - Home.module.scss
 
-routeName = 'home'
 fileRoutes = ['module','page','layout']
 def createRouteFiles(routeName, fileNames, path):
+    routeName = capitalizeCamelCase(routeName)
     for name in fileNames:
         if name == "module":
-            routeName = routeName.capitalize()
             createFile(f'{routeName}.txt',"")
             changeExtension(f'{routeName}.txt', 'scss', module=True)
             shutil.move(f'./{routeName}.module.scss',f'{path}/{routeName}.module.scss')
@@ -59,19 +61,18 @@ def createRouteFiles(routeName, fileNames, path):
             shutil.move(f'./{name}.tsx',f'{path}/{name}.tsx')
     return True
 
-createRouteFiles(routeName, fileRoutes, "./src/app")
-
 # PASO 3: CREAMOS LAS CARPETAS DE RUTAS
 
 toCreate = getData("./toCreate.json")
 
 routesToCreate = toCreate["routes"]
-folderRoutes = "./src/app"
+folderRoutes = f"{proyectPath}/app"
+
 def createRoutes(routeList, folderPath):
     for route in routeList:
-        path = f'{folderPath}/{route["name"]}'
+        path = f'{folderPath}/{route["name"].lower()}'
         os.makedirs(path)
-        createRouteFiles(route["name"], fileRoutes, path)
+        createRouteFiles(route["fallback-name"] if route.get('fallback-name') else route['name'], fileRoutes, path)
         if route["subroutes"]:
             createRoutes(route["subroutes"], path)
     return True
@@ -81,21 +82,21 @@ createRoutes(routesToCreate, folderRoutes)
 # PASO 4: CREAMOS LAS CARPETAS DE RUTAS
 
 componentsToCreate = toCreate["components"]
-compPath = "./src/components"
+compPath = f"{proyectPath}/components"
 fileComponents = ["template","functional","module"]
 
 def createComponentsFiles(cName, fileNames, path):
-    for name in fileNames:
-        if name == "module":
+    for fileType in fileNames:
+        if fileType == "module":
             createFile(f'{cName}.txt',"")
             changeExtension(f'{cName}.txt', 'scss', module=True)
-            shutil.move(f'./{cName}.module.scss',f'{path}/{cName}.module.scss')
-        elif name == "template":
-            createFile(f'{cName}.txt', writeText(cName, name))
+            shutil.move(f'./{cName}.module.scss',f'{path}/{cName}.module.scss') 
+        elif fileType == "template":
+            createFile(f'{cName}.txt', writeText(cName, fileType))
             changeExtension(f'{cName}.txt', 'tsx', template=True)
             shutil.move(f'./{cName}.template.tsx',f'{path}/{cName}.template.tsx')
         else:
-            createFile(f'{cName}.txt', writeText(cName, name))
+            createFile(f'{cName}.txt', writeText(cName, fileType))
             changeExtension(f'{cName}.txt', 'ts')
             shutil.move(f'./{cName}.ts',f'{path}/{cName}.ts')
     return True
